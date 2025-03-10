de6fdf24a39ad3f3dbfc8ee51a11dd737bb92c17
rizkiarch <muhamadrizki1a@gmail.com>
2025-03-10 20:32:55 +0700

    Initialize Node.js project with Express and dotenv configuration

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+
+// Create Express app
+const app = express();
+const port = process.env.PORT;
+
+// Middleware untuk parsing JSON
+app.use(express.json());
+
+// Endpoint for testing server
+app.get('/', (req, res) => {
+    res.send('Server berhasil berjalan');
+});
+
+// Jalankan server Express
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
